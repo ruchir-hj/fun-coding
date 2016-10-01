102300d9a816fc31b017ab5c6c398c3faff8c9d8
Ruchir Hajela <ruchirhajela@dhcp-gs-3419.redrover.cornell.edu>
2016-10-01 16:53:23 -0400

    added more problems

---
+++ leetcode solutions/C++/string to integer (atoi)/atoi.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+
+        if (s.length() == 0) {
+            return 0;
+        }
+
+        int sign = 1, base = 0, i = 0;
+        while (s[i] == ' ') { i++; }
+
+        if (s[i] == '-' || s[i] == '+') {
+            sign = 1 - 2 * (s[i++] == '-');
+        }
+
+        while (s[i] >= '0' && s[i] <= '9') {
+            if (base > INT_MAX / 10 || (base == INT_MAX / 10 && s[i] - '0' > 7)) {
+                if (sign == 1) return INT_MAX;
+                else return INT_MIN;
+            }
+            base = 10 * base + (s[i++] - '0');
+        }
+        return base * sign;
+
+    }
+
+};
